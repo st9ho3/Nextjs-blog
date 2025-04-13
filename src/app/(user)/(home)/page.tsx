e283f86666b5c9e92982974dd386b1beaac8e704
st9ho3 <stahos21@gmail.com>
2025-04-13 21:50:23 +0300

    SSR on home

--- src/app/(user)/(home)/page.tsx
+++ src/app/(user)/(home)/page.tsx
@@ -5,13 +5,15 @@ import '../../(components)/header.css';
 import Sidebar from '@/app/(components)/sidebar';
 import { getArticles } from '@/app/_lib/utils';
 
+export const dynamic = 'force-dynamic';
 
 const page = async () => {
   const data = await getArticles();
+  console.log('Data:', data);
   return (
     <div className="homepage">
       {<div className="home">
-        {data.length > 0 ? data.map((article) => (
+        {data.length > 0 ? data.map((article: Article) => (
           <ArticlePreview key={article.id} article={article} />
         )): <h2>Loading Articles...</h2> }
       </div>}
